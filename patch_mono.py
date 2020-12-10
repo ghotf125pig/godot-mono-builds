f49f98fdfb58874320291d3738f7a06210b76fb7
邓坚 <dj4156003@gmail.com>
2020-12-10 13:12:10 +0800

    临时打开mono thread状态机的debug开关

--- patch_mono.py
+++ patch_mono.py
@@ -34,6 +34,7 @@ def main(raw_args):
         'mono_osx_threads_helper_unity.diff',
         'mini_runtime_remove_interp_error.diff',
         'mono_transform_error_leak.diff',
+        'mono-thread-fsm-debug-enabled.diff',
     ]
 
     if os.path.isfile(os.path.join(mono_source_root, 'mono/tools/offsets-tool/offsets-tool.py')):
