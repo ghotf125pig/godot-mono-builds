7786d64d88f5b5092cb6f08dca4f1ade65429c03
邓坚 <dj4156003@gmail.com>
2021-01-09 16:16:45 +0800

    add mono fm utils

--- patch_mono.py
+++ patch_mono.py
@@ -34,6 +34,7 @@ def main(raw_args):
         'mono_osx_threads_helper_unity.diff',
         'mini_runtime_remove_interp_error.diff',
         'mono_transform_error_leak.diff',
+        'add-mono-fm-utils.diff',
     ]
 
     if os.path.isfile(os.path.join(mono_source_root, 'mono/tools/offsets-tool/offsets-tool.py')):
